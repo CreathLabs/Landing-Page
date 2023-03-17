b189e1c36279f581e25626dd430e27d08137b784
GodshieldOche <ggodshield@gmail.com>
2023-03-17 12:04:32 +0100

    [Chore] Fix

--- components/Layout/Menu.tsx
+++ components/Layout/Menu.tsx
@@ -4,7 +4,7 @@ import NavLink from "../Common/NavLink";
 const Menu: React.FC<{ open: boolean }> = ({ open }) => {
   return (
     <div
-      className="fixed xl:hidden top-0 overflow-hidden left-0 right-0 bottom-0 menu_bg z-40 transition-all duration-300 ease-in-out "
+      className="fixed xl:hidden top-0 overflow-hidden left-0 right-0 bottom-0 menu_bg z-40 transition-all duration-700 ease-in-out "
       style={{
         height: open ? "100vh" : "0px",
       }}
