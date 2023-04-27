a8012d18c89351fa5a6f162636ffda73e03d8f5e
GodshieldOche <ggodshield@gmail.com>
2023-04-27 08:39:54 +0100

    [Fix]: Partners url

--- components/Home/Partners.tsx
+++ components/Home/Partners.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const partners = [
   {
     logo: "https://res.cloudinary.com/dk6uhtgvo/image/upload/v1681110380/Brokenegg_Logo_White_2_pheyhy.png",
-    link: "",
+    link: "https://www.brokenegg.io",
   },
   {
     logo: "https://res.cloudinary.com/dk6uhtgvo/image/upload/v1681110380/Sportrex_logo_and_logo_mark_white_1_dmqso0.png",
-    link: "",
+    link: "https://www.sportrex.io",
   },
 ];
 
@@ -18,7 +19,9 @@ const Partners = () => {
       <h1 className=" text-[22px] md:text-3xl font-Playfair  ">Our Partners</h1>
       <div className="flex w-full max-w-[1155px] mx-auto justify-center space-x-10 items-center scroller overflow-x-auto">
         {partners.map((item, index) => (
-          <div
+          <Link
+            href={item.link}
+            target="_blank"
             key={index}
             className=" w-full max-w-[145px]  md:max-w-[180px] relative h-[65px] md:h-[90px]  "
           >
@@ -28,7 +31,7 @@ const Partners = () => {
               className=" object-contain "
               alt="Partner"
             />
-          </div>
+          </Link>
         ))}
       </div>
     </div>
