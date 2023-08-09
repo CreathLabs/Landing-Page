d169e939367e1a1708266f917c66732e17c76d32
GodshieldOche <ggodshield@gmail.com>
2023-08-09 22:27:42 +0100

    [Chore]: Added SEO descriptions and keywords

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,14 @@ export default function HomePage() {
     <>
       <Head>
         <title>Creath</title>
-        <meta name="description" content="Creath" />
+        <meta
+          name="description"
+          content="Creath here Digital meets Physical Creativity meets Innovation"
+        />
+        <meta
+          name="keywords"
+          content="Creath, Art Marketplace, Web3 Company, Blockchain, Creath Labs, Digital Gallery, Technology, Creath and Creatives, San Jose, CA, Lagos, Art, Where Digital meets Physical, Creativity meets Innovation,"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
