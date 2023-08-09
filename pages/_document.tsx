2386764c32eb0fe44ffd40e3e3c305c4a7102036
GodshieldOche <ggodshield@gmail.com>
2023-08-09 22:34:42 +0100

    [Chore]: Added SEO descriptions and keywords

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en" className="!scroll-smooth !scroll-pt-[150px]">
       <Head>
+        <meta
+          name="description"
+          content="Creath here Digital meets Physical Creativity meets Innovation"
+        />
+        <meta
+          name="keywords"
+          content="Creath, Art Marketplace, Web3 Company, Blockchain, Creath Labs, Digital Gallery, Technology, Creath and Creatives, San Jose, CA, Lagos, Art, Where Digital meets Physical, Creativity meets Innovation,"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-4PKDEV8KMW"
